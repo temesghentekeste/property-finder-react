fff8ea6800015679eadf2e73993636efac0a557a
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-14 00:14:22 +0300

    Fetch properties from the back-end api

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,26 @@
+/* eslint-disable no-unused-vars */
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { WaveLoading } from 'react-loadingg';
 import Properties from '../../containers/properties';
 import styles from './App.module.css';
 
+import {
+  getPropertiesAsync,
+} from '../../redux/propertiesSlice';
+
 function App() {
+  const dispatch = useDispatch();
+  const { loading, properties } = useSelector(
+    (state) => state.properties,
+  );
+
+  useEffect(() => {
+    dispatch(getPropertiesAsync());
+  }, [dispatch]);
+
+  console.log(properties);
   return (
     <div className={styles.App}>
       <h1>Property Finder</h1>
