17f890f6d9b2538797214a47e9d088bbc362c87a
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-13 22:54:54 +0300

    Add Properties container

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,11 @@
+import Properties from '../../containers/properties';
 import styles from './App.module.css';
 
 function App() {
   return (
     <div className={styles.App}>
       <h1>Property Finder</h1>
+      <Properties />
     </div>
   );
 }
---
+++ src/containers/properties/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Properties = () => (
+  <div>
+    <h2>All Properties</h2>
+  </div>
+);
+
+export default Properties;
