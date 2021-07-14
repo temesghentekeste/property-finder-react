95829bc25392bd6774ee8bfbab495ad06b46cecc
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-14 16:10:41 +0300

    Add login reducer to the global store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import propertiesReducer from '../redux/propertiesSlice';
 import propertyReducer from '../redux/propertySlice';
+import loginReducer from '../redux/loginSlice';
 
 export default configureStore({
   reducer: {
     properties: propertiesReducer,
     property: propertyReducer,
+    login: loginReducer,
   },
 });
