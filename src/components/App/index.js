7ea113e2ae00922a0c962615776e0eb9dc98f125
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-14 11:41:46 +0300

    Add property componet inside properties container; refactor code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,31 +1,12 @@
 /* eslint-disable no-unused-vars */
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-
-import { WaveLoading } from 'react-loadingg';
 import Properties from '../../containers/properties';
 import styles from './App.module.css';
 
-import { getPropertiesAsync } from '../../redux/propertiesSlice';
-
 function App() {
-  const dispatch = useDispatch();
-  const { loading, properties } = useSelector((state) => state.properties);
-
-  useEffect(() => {
-    dispatch(getPropertiesAsync());
-  }, [dispatch]);
-
-  console.log(properties);
-
-  if (loading || loading === null || loading === undefined) {
-    return <WaveLoading />;
-  }
-
   return (
     <div className={styles.App}>
       <h1>Property Finder</h1>
-      <Properties properties={properties} />
+      <Properties />
     </div>
   );
 }
--- src/containers/properties/index.js
+++ src/containers/properties/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-const Properties = () => (
-  <div>
-    <h2>All Properties</h2>
-  </div>
-);
+import { WaveLoading } from 'react-loadingg';
+import { getPropertiesAsync } from '../../redux/propertiesSlice';
+
+const Properties = () => {
+  const dispatch = useDispatch();
+  const { loading, properties } = useSelector((state) => state.properties);
+
+  useEffect(() => {
+    dispatch(getPropertiesAsync());
+  }, [dispatch]);
+
+  console.log(properties);
+
+  if (loading || loading === null || loading === undefined) {
+    return <WaveLoading />;
+  }
+  return (
+    <div>
+      <h2>All Properties</h2>
+    </div>
+  );
+};
 
 export default Properties;
