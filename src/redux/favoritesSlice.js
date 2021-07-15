3c8a11962e77f3b1d449fa0bd38681a37fe51518
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-15 19:46:08 +0300

    Add favorites slice and update the store

---
+++ src/redux/favoritesSlice.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-param-reassign */
+import axios from 'axios';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { axiosDefaults, axiosHeders } from '../api/axiosParams';
+
+const initialState = {
+  loading: false,
+  favorites: [],
+  error: '',
+};
+
+export const getFavoritesAsync = createAsyncThunk(
+  'favorites/getFavoritesAsync',
+  async (username) => {
+    await axiosDefaults();
+    await axiosHeders();
+    const response = await axios.get(`/favorires/${username}`);
+    const favorites = await response.data;
+    return favorites;
+  },
+);
+
+const favoritesSlice = createSlice({
+  name: 'favorites',
+  initialState,
+  extraReducers: {
+    [getFavoritesAsync.pending]: (state, action) => {
+      state.loading = true;
+      state.favorites = [];
+      state.error = '';
+    },
+    [getFavoritesAsync.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.favorites = action.payload;
+      state.error = '';
+    },
+    [getFavoritesAsync.rejected]: (state, action) => {
+      state.loading = false;
+      state.favorites = [];
+      state.error = 'Something went wrong, please log in and try again!';
+    },
+  },
+});
+
+export default favoritesSlice.reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import propertiesReducer from '../redux/propertiesSlice';
 import propertyReducer from '../redux/propertySlice';
 import loginReducer from '../redux/loginSlice';
+import favoritesReducer from '../redux/favoritesSlice';
 
 export default configureStore({
   reducer: {
     properties: propertiesReducer,
     property: propertyReducer,
     usernametoken: loginReducer,
+    favorites: favoritesReducer,
   },
 });
