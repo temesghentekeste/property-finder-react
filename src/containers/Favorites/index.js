b0a92a76ca71021171a8eb6694dc6a964b32e312
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-15 19:24:54 +0300

    Add favorite container component

---
+++ src/containers/Favorites/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Favorites = () => {
+    console.log('favorires')
+    return (
+        <div>
+           <h1>Favorites</h1> 
+        </div>
+    )
+}
+
+export default Favorites
