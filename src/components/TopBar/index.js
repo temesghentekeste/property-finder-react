f0d06dd63392eadd6fed0c7ae82abac8ba2706d3
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-20 21:25:07 +0300

    Add topBar component

---
+++ src/components/TopBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './TopBar.module.css'
+
+const TopBar = () => {
+    return (
+        <div className={styles.topBarContainer}>
+            Hi
+        </div>
+    )
+}
+
+export default TopBar
